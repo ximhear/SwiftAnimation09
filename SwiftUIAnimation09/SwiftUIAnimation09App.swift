c2096cb11f11cd81e84dca6ee3b10dd49fb7b04a
chlee <z000000375@wemakeprice.com>
2023-04-04 01:30:54 +0900

    Initial Commit

---
+++ SwiftUIAnimation09/SwiftUIAnimation09App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimation09App.swift
+//  SwiftUIAnimation09
+//
+//  Created by gzonelee on 2023/04/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimation09App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
